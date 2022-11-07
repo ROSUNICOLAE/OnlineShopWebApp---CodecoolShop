712a54cf81a9660db3e5650a6ac7feca46fc683b
904756 <904756@gmail.com>
2022-11-07 16:16:59 +0200

    Add new category of product and product itself

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -27,10 +27,13 @@ public class Initializer implements ServletContextListener {
         //setting up a new supplier
         Supplier amazon = new Supplier("Amazon", "Digital content and services");
         supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
+        Supplier lenovo = new Supplier("Lenovo", "Laptop");
         supplierDataStore.add(lenovo);
         Supplier apple = new Supplier("Apple", "Phones");
         supplierDataStore.add(apple);
+        Supplier hp = new Supplier("HP", "Laptop");
+        supplierDataStore.add(hp);
+
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware",
@@ -39,8 +42,10 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(tablet);
         ProductCategory ctg2 = new ProductCategory("ctg2", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(ctg2);
-        ProductCategory phones = new ProductCategory("Phones", "", "Smart phones");
-        productCategoryDataStore.add(phones);
+        ProductCategory phone = new ProductCategory("Phones", "", "Smart phones");
+        productCategoryDataStore.add(phone);
+        ProductCategory laptop = new ProductCategory("Laptop", "", "Laptop");
+        productCategoryDataStore.add(laptop);
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
@@ -50,6 +55,8 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Amazonn Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", ctg2, amazon));
         productDataStore.add(new Product("Lenovoo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ctg2, lenovo));
         productDataStore.add(new Product("Amazonn Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ctg2, amazon));
-        productDataStore.add(new Product("Telefon mobil Apple iPhone 11, 64GB, Red", new BigDecimal("2"), "EUR", "New system with two cameras. Battery for the whole day. The most durable glass ever used in a smartphone. Apple's fastest processor ever.", phones, apple));
+        productDataStore.add(new Product("Telefon mobil Apple iPhone 11, 64GB, Red", new BigDecimal("2"), "EUR", "New system with two cameras. Battery for the whole day. The most durable glass ever used in a smartphone. Apple's fastest processor ever.", phone, apple));
+        productDataStore.add(new Product("Laptop HP Pavilion Aero 13", new BigDecimal("24"), "EUR", "Laptop HP Pavilion Aero 13-be0031nq, AMD Ryzen 5 5600U pana la 4.2 GHz, 13.3\"," +
+                " WUXGA IPS, 16GB, SSD 512GB, AMD Radeon Graphics, Windows 11 Home, Pale Rose Gold", laptop, hp));
     }
 }
