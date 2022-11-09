a939ff2de4e7f806f7bd55a290bcae80fa706799
rosun <edglobalsolutions@gmail.com>
2022-11-09 13:27:51 +0200

    some minor changes added

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -41,17 +41,17 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(tablet);
         ProductCategory laptop = new ProductCategory("Laptop","Hardware","Portable power");
         productCategoryDataStore.add(laptop);
-        ProductCategory smartphone = new ProductCategory("smartphone","Hardware","Smart at your fingers");
-        productCategoryDataStore.add(smartphone);
+        ProductCategory Smartphone = new ProductCategory("smartphone","Hardware","Smart at your fingers");
+        productCategoryDataStore.add(Smartphone);
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon, "/static/img/product_1.jpg"));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo, "/static/img/product_2.jpg"));
         productDataStore.add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon, "/static/img/product_3.jpg"));
         productDataStore.add(new Product("MacBook Noob",new BigDecimal("1200"),"USD","test description",laptop,amazon,"https://lcdn.altex.ro/resize/media/catalog/product/M/a/2bd48d28d1c32adea0e55139a4e6434a/MacBook_Pro_13in_Silver-1.jpg"));
         productDataStore.add(new Product("TEST",new BigDecimal("1000"),"USD","test description",laptop,amazon,"/static/img/test.jpeg"));
-        productDataStore.add(new Product("Phone1",new BigDecimal("1000"),"USD","Smarphone",smartphone,apple,"/static/img/1.jpeg"));
-        productDataStore.add(new Product("Phone2",new BigDecimal("1001"),"USD","Smarphone",smartphone,apple,"/static/img/2.jpeg"));
-        productDataStore.add(new Product("Phone3",new BigDecimal("1002"),"USD","Smarphone",smartphone,apple,"/static/img/3.png"));
+        productDataStore.add(new Product("Phone1",new BigDecimal("1000"),"USD","Smarphone",Smartphone,apple,"/static/img/1.jpeg"));
+        productDataStore.add(new Product("Phone2",new BigDecimal("1001"),"USD","Smarphone",Smartphone,apple,"/static/img/2.jpeg"));
+        productDataStore.add(new Product("Phone3",new BigDecimal("1002"),"USD","Smarphone",Smartphone,apple,"/static/img/3.png"));
         //creating an admin
         userDaoStore.addUser("admin", "admin", "admin@email.com", Role.ADMIN);
     }
